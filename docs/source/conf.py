23e017f7f40c65b9ab179b8a9f1b44ecc6bec6f7
sakshi-agnisys <47734944+sakshi-agnisys@users.noreply.github.com>
2024-04-25 13:07:30 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Agnisys'
+copyright = '© 2007 - 2023 Agnisys® Inc. All Rights Reserved. https://www.agnisys.com/submit-feedback/'
+author = 'Agnisys'
 
 release = '0.1'
 version = '0.1.0'
